c0e4df6656330167824823cac9ff44bf9d7344ae
carl-pki <Zhaolin.Zhang@PERKINELMER.COM>
2021-12-12 16:11:04 +0800

    init

---
+++ src/main/java/com/qing/bai/Test.java
@@ -0,0 +1,13 @@
+package com.qing.bai;
+
+/**
+ * @Classname com.qing.bai.Test
+ * @Date 12/12/2021 4:09 PM
+ * @Created by ZhangZ25724
+ * @Description TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
--- src/main/scala/com/qing/bai/Test.scala
+++ src/main/scala/com/qing/bai/Test.scala
@@ -1,6 +1,5 @@
 package com.qing.bai
 
-
 object Test {
   def main(args: Array[String]) {
     val strings = Set("1", "2", "immutable")
