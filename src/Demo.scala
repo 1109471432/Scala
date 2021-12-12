c8e18ac6800b88ffad051afc099990e0b329e6a7
1109471432 <1109471432@qq.com>
2021-12-12 13:19:08 +0800

    init

---
+++ src/Demo.scala
@@ -0,0 +1,10 @@
+package com.qing.bai
+
+class Demo {
+  private var name:String = "1";
+  private var age:String = "1";
+
+  def add(a: Int, b: Int): Int = {
+    return a + b;
+  }
+}
---
+++ src/Test.scala
@@ -0,0 +1,9 @@
+package com.qing.bai
+
+
+object Test {
+  def main(args: Array[String]) {
+    val tupleA = (1, 2, 3, "1231")
+    print(tupleA)
+  }
+}
