37830f2a44484d5a4a0b025f6ede1c41508696d0
1109471432 <1109471432@qq.com>
2021-12-12 15:42:51 +0800

    init

--- src/Test.scala
+++ src/Test.scala
@@ -3,7 +3,7 @@ package com.qing.bai
 
 object Test {
   def main(args: Array[String]) {
-    val tupleA = (1, 2, 3, "1231")
-    print(tupleA)
+    val strings = Set("1", "2", "immutable")
+    println(strings)
   }
 }
